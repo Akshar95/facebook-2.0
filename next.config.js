d3da7ea65b3865b0ea512f9363c76ee0d4303544
Akshar Patel <patel.akshar295@gmail.com>
2021-06-13 23:51:15 +0100

    create next.config.js file to whitelist images src

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// white list domains to use in Image component for security reasons
+module.exports ={
+    images: {
+        domains: [
+            'links.papareact.com',
+            'platform-lookaside.fbsbx.com', //where FB stores their pictures
+            'firebasestorage.googleapis.com',
+
+        ]
+    }
+}
